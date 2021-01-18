46e17b3a474f0bceea8fc91f6b5833a1ffee1262
Kanat Aidarov <kanataidarov@yahoo.com>
2021-01-18 21:58:59 +0600

    travelPaths added

--- cities/models.py
+++ cities/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 class City(models.Model):
-    name = models.TextField()
+    name = models.TextField(unique=True)
+    order = models.IntegerField(unique=True, default=0)
     distances = models.TextField()
+
+class CityResponse(models.Model): 
+    startCity = models.TextField()
+    pathWeight = models.IntegerField()
+    path = models.TextField() 
+
--- cities/schema.py
+++ cities/schema.py
@@ -1,41 +1,73 @@
 import graphene
 from graphene_django import DjangoObjectType 
-from graphene_django.filter import DjangoFilterConnectionField 
-from .models import City 
-import django_filters 
+from .models import City, CityResponse 
+from graphql import GraphQLError
+from itertools import permutations
 
-class CityFilter(django_filters.FilterSet): 
+class CityType(DjangoObjectType): 
     class Meta: 
         model = City 
-        fields = ['name', 'distances'] 
 
-class CityNode(DjangoObjectType): 
+class CityResponseType(DjangoObjectType): 
     class Meta: 
-        model = City 
-        interfaces = {graphene.relay.Node, } 
+        model = CityResponse 
+
+DEFAULT_CITIES = ['Нур-Султан',  'Алматы',  'Шымкент',  'Актобе',  'Атырау', 'Усть-Каменогорск']
 
 class Query(graphene.ObjectType): 
-    city = graphene.relay.Node.Field(CityNode) 
-    cities = DjangoFilterConnectionField(CityNode, filterset_class=CityFilter) 
+    cities = graphene.List(CityType)
+    travel_paths = graphene.List(CityResponseType, startAt=graphene.String(), requestedCities=graphene.List(graphene.String)) 
 
-class CreateCity(graphene.relay.ClientIDMutation):
-    city = graphene.Field(CityNode)
+    def resolve_cities(self, info, **kwargs): 
+        return City.objects.all() 
 
-    class Input:
+    def resolve_travel_paths(self, info, startAt=None, requestedCities=DEFAULT_CITIES, **kwargs): 
+        startCity = City.objects.filter(name=startAt).first() 
+        if not startCity: 
+            raise GraphQLError('You must provide correct starting city name')
+
+        graph = []
+        graph.append([int(d) for d in startCity.distances.split(',')])
+
+        for city in City.objects.all():
+            if city.name == startCity.name: 
+                continue 
+            graph.append([int(d) for d in city.distances.split(',')])
+
+        paths = travelPaths(graph, City.objects, requestedCities)
+
+        response = []
+        for key in sorted(paths.keys(), reverse=True): 
+            response.append( 
+                CityResponse( 
+                    startCity = startCity.name, 
+                    pathWeight = key, 
+                    path = paths[key]
+                ) 
+            )
+
+        return response
+
+class CreateCity(graphene.Mutation):
+    city = graphene.Field(CityType)
+
+    class Arguments:
         name = graphene.String()
+        order = graphene.Int() 
         distances = graphene.String() 
 
-    def mutate_and_get_payload(root, info, **input): 
+    def mutate(self, info, name, order, distances): 
         city = City(
-            name=input.get('name'), 
-            distances=input.get('distances')
+            name=name, 
+            order=order, 
+            distances=distances
         )
         city.save()
 
         return CreateCity(city=city)
 
 class DeleteCities(graphene.Mutation):
-    cities = graphene.List(CityNode)
+    cities = graphene.List(CityType)
 
     class Arguments:
         name = graphene.String()
@@ -50,3 +82,28 @@ class DeleteCities(graphene.Mutation):
 class Mutation(graphene.AbstractType): 
     create_city=CreateCity.Field()
     delete_cities=DeleteCities.Field() 
+
+def travelPaths(graph, cities, requestedCities, s=0): 
+    vertices = [] 
+    for requestedCity in requestedCities: 
+        city = cities.filter(name=requestedCity).first()
+        if not city: 
+            raise GraphQLError('Requested city not present in database')
+
+        if city.order != s: 
+            vertices.append(city.order) 
+ 
+    paths = {}
+    for combination in permutations(vertices):
+        curr_weight = 0
+        curr_path = []
+        k = s 
+        for j in combination: 
+            curr_weight += graph[k][j] 
+            curr_path.append(cities.filter(order=k).first().name)
+            k = j 
+        curr_weight += graph[k][s] 
+        curr_path.append(cities.filter(order=k).first().name)
+        paths[curr_weight]=curr_path
+         
+    return paths 
